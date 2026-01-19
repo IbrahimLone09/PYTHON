0ca0cba8a8d2d95b8a94a55273a1f13878e77cae
IbrahimLone09 <ibrahimnazir00003120401@gmail.com>
2026-01-19 19:54:51 +0530

    Add new features

--- greaterorsmaller.py
+++ greaterorsmaller.py
@@ -2,6 +2,6 @@
 number is greater than print true
 if not than print false"""
 
-a=int(input("Enter first number :"))
-b=int(input("Enter second  number :"))
+a=int(input("Enter first number  :"))
+b=int(input("Enter second number:"))
 print(a>=b)
